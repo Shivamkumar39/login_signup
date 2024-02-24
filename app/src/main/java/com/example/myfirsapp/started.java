be987942d50c94c4c0e87cbeac4f4d8c3ae8427f
Shivamkumar098798 <shivamkumar098798@gmail.com>
2024-02-24 21:37:47 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myfirsapp/started.java
@@ -0,0 +1,14 @@
+package com.example.myfirsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class started extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
